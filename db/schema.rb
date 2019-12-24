06d0b3a68fc736a02d8757c97127f850cf453795
Dipak <dade.dipakg@gmail.com>
2019-12-24 20:39:45 +0530

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_12_24_081403) do
+
+  create_table "hits_records", force: :cascade do |t|
+    t.integer "search_query_id"
+    t.integer "last_day"
+    t.integer "last_week"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["search_query_id"], name: "index_hits_records_on_search_query_id"
+  end
+
+  create_table "search_notebooks", force: :cascade do |t|
+    t.string "title"
+    t.string "creation_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "search_query_id"
+    t.index ["search_query_id"], name: "index_search_notebooks_on_search_query_id"
+  end
+
+  create_table "search_queries", force: :cascade do |t|
+    t.string "query_string"
+    t.string "creation_date"
+    t.string "total_hits"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "search_results", force: :cascade do |t|
+    t.string "hn_author"
+    t.integer "author_karma_points"
+    t.string "url"
+    t.string "creation_date"
+    t.string "hn_tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "search_query_id"
+    t.integer "search_notebook_id"
+    t.index ["search_notebook_id"], name: "index_search_results_on_search_notebook_id"
+    t.index ["search_query_id"], name: "index_search_results_on_search_query_id"
+  end
 
 end
