8ea6f669d5cdbb3cddf3401d188efae97a587008
Dipak <dade.dipakg@gmail.com>
2019-12-24 20:36:40 +0530

    modification in config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
+
+  resources :notebooks  do
+    member do
+      delete 'destroy_result'
+    end
+    collection do
+      post 'statistics'
+    end
+  end
+ 
+  resources :search_results do 
+    member do 
+      get 'add'
+    end
+  end
+
+  resources :hn_search_notebooks do 
+    collection do 
+      get 'search'
+    end
+  end
+
+  root to: 'notebooks#statistics'
 end
---
+++ config/schedule.rb
@@ -0,0 +1,3 @@
+every 1.day, at: '12:30 am' do
+  rake "get_query_statistics:time_based_data"
+end
